3084fc4b53631432729401d720d42805fce30e43
Parker Zink <parker.zink@gmail.com>
2015-01-14 09:12:16 -0800

    Gave public functions a message
    Gave my public function classes each with a message.

--- index.php
+++ index.php
@@ -1,49 +1,62 @@
 <?php 
 class MyExample1 {
-  protected $name;
-  protected $age;
-  protected $mascot;
+  public $name;
+  public $age;
+  public $mascot;
 
   public function __construct ( $name, $age, $mascot ) {
     $this->name = $name;
     $this->age = $age;
     $this->mascot = $mascot;
   }
-}
 
+
+public function message() {
+return "Hello world! My name is " . $this->name . ", and I am a " . $this->age . "-year old " . $this->mascot . "!";
+}
+}
 // and you would use this as:
 $myexample1 = new MyExample1 ( 'Parker', '16', 'bulldog' );
+echo $myexample1->message();
 ?>
 
 <?php 
 class MyExample2 {
-  protected $grade;
-  protected $school;
-  protected $district;
+  public $grade;
+  public $school;
+  public $district;
 
   public function __construct ( $grade, $school, $district ) {
     $this->grade = $grade;
     $this->school = $school;
     $this->district = $district;
   }
-}
+ public function message2() {
+              return "I am also a " . $this->grade . " grader at " . $this->school . ", which is associated with the " . $this->district . ".";
+            }
+          }
 
 // and you would use this as:
-$myexample2 = new MyExample ( '10th', 'Pasadena High School', 'Pasadena Unified School District' );
+$myexample2 = new MyExample2 ( '10th', 'Pasadena High School', 'Pasadena Unified School District' );
+echo $myexample2->message2();
 ?>
 <?php 
 class MyExample3 {
-  protected $startingyear;
-  protected $endingyear;
-  protected $yearsleft;
+  public $startingyear;
+  public $endingyear;
+  public $yearsleft;
 
   public function __construct ( $startingyear, $endingyear, $yearsleft ) {
     $this->startingyear = $startingyear;
     $this->endingyear = $endingyear;
     $this->yearsleft = $yearsleft;
   }
-}
+ public function message3() {
+              return "I have started school in " . $this->startingyear . " and will soon be ended in  " . $this->endingyear . ". So that means that I have only " . $this->yearsleft . " years left until I leave PHS.";
+            }
+          }
 
 // and you would use this as:
-$myexample3 = new MyExample ( '2014', '2017', '2' );
+$myexample3 = new MyExample3 ( '2014', '2017', '2' );
+echo $myexample3->message3();
 ?>
